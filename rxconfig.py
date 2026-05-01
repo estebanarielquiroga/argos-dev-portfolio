065770ed5ee4cf9ef72b6b8435f5e26c0964f7e0
eaqDev <estebanarielquiroga@gmail.com>
2026-05-01 15:53:04 -0300

    fix: configurar backend_host y simplificar proxy para estabilidad de websockets

--- rxconfig.py
+++ rxconfig.py
@@ -3,5 +3,6 @@ import reflex as rx
 config = rx.Config(
     app_name="argos_dev_portfolio",
     api_url="https://quirodev.ar",
-    # En 0.9.x el manejo de plugins es mas limpio, dejamos la config base
+    # Forzamos al backend a escuchar en todas las interfaces internas
+    backend_host="0.0.0.0",
 )
